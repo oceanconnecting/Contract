e1c48036c218657c8027c327f2ea3fbdc113a6b7
mostafa <mostafaakajdid6@gmail.com>
2024-11-14 18:03:23 +0100

    mos

--- app/components/ui/card.jsx
+++ app/components/ui/card.jsx
@@ -1,6 +1,6 @@
 export function Card({ children, className, ...props }) {
     return (
-      <div className={`p-4 bg-white shadow rounded ${className}`} {...props}>
+      <div className={`p-4 bg-blue100 shadow rounded ${className}`} {...props}>
         {children}
       </div>
     )
