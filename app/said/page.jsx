02080799853bf18e2e64e73b51acbcc4292236cc
mostafa <mostafaakajdid6@gmail.com>
2024-11-30 12:02:12 +0100

    mos

---
+++ app/said/page.jsx
@@ -0,0 +1,14 @@
+"use client"; 
+
+import SignInComponent from '../components/espace-client/login';
+import ProfileDashboard from '../components/profile/profile';
+
+
+export default function Said() {
+  return (
+    <div>
+      {/* <SignInComponent /> */}
+      <ProfileDashboard/>
+    </div>
+  );
+}
