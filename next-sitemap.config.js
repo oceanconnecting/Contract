95227a6a61dafeea44a2a6f4ffef33289ce1c615
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2025-03-06 15:22:35 +0000

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'oceanconnecting.org', // Replace with your actual domain
+    generateRobotsTxt: true, // Generates a robots.txt file
+    sitemapSize: 5000, // (optional) Max number of URLs per sitemap file
+  };
+  
